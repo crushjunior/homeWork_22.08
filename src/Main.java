69609f447bac9aefaee71ada2eb690d0aec7e00e
crushjunior <94167663+crushjunior@users.noreply.github.com>
2022-08-24 18:39:59 +0300

    3 Tasks. Ver 2.0

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        isLeapYear(2024);
+        printLeapYear(2024);
         System.out.println();
-        getClient(1, LocalDate.now().getYear());
+        printClient(5, LocalDate.now().getYear());
         System.out.println();
-        timeOfDelivery(45);
+        System.out.println("Потребуется дней: " + calculateDeliveryDays(78));
 
     }
 
     // Task 1
-    public static void isLeapYear(int year) {
+    private static void printLeapYear(int year) {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " високосный год");
         } else {
@@ -19,30 +19,39 @@ public class Main {
         }
     }
     // Task 2
-    public static void getClient(int oc, int currentYear) {
-            if (oc == 1) {
-                if (currentYear >= 2015) {
+    private static void printClient(int oc, int currentYear) {
+            switch (oc) {
+                case 1 :
+                    if (currentYear >= 2015) {
                     System.out.println("Установите версию приложения для Android по ссылке.");
-                } else {
+                    } else {
                     System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-                }
-            } else if (currentYear >= 2015) {
-                System.out.println("Установите версию приложения для iOS по ссылке.");
-            } else {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+                    }
+                break;
+                case 0 :
+                    if (currentYear >= 2015) {
+                        System.out.println("Установите версию приложения для iOS по ссылке.");
+                    } else {
+                        System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+                    }
+                break;
+                default :
+                    System.out.println("Ошибка!");
             }
+
     }
     // Task 3
-    public static void timeOfDelivery(int deliveryDistance) {
-
+    private static int calculateDeliveryDays(int deliveryDistance) {
+        int timeDelivery = 0;
         if (deliveryDistance <= 20 && deliveryDistance >0) {
-            System.out.println("Потребуется дней: 1");
+            timeDelivery = 1;
         }
         if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            System.out.println("Потребуется дней: 2");
+            timeDelivery = 2;
         }
         if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            System.out.println("Потребуется дней: 3");
+            timeDelivery = 3;
         }
+        return timeDelivery;
     }
 }
